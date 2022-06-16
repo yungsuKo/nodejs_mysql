14c8dc6881751688e30b354ff47ce97cc8f44cf1
Tom <yungsu2391@gmail.com>
2022-06-17 07:50:07 +0900

    author create end

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var template = require('./lib/template.js');
 var path = require('path');
 var db = require('./lib/db.js')
 var topic = require('./lib/topic')
+var author = require('./lib/author')
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -26,6 +27,10 @@ var app = http.createServer(function(request,response){
       topic.update_process(request, response)
     } else if(pathname === '/delete_process'){
       topic.delete_process(request, response)
+    } else if(pathname === '/author'){
+      author.home(request, response)
+    } else if(pathname === '/author/create_process'){
+      author.create_process(request, response)
     } else {
       response.writeHead(404);
       response.end('Not found');
